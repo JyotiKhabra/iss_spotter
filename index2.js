420a48f4c05e341beb39bd7ed3f71157c0bc3c5b
vagrant <j_khabra11@hotmail.com>
2020-04-10 04:22:33 +0000

    promises with ISS

---
+++ index2.js
@@ -0,0 +1,10 @@
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("Not working as planned: ", error.message);
+  });
